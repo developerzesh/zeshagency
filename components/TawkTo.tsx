b0466140fac39f9bb2aea374b1a19e3e68da48d5
devsprucelifeskills <dev.sprucelifeskills@gmail.com>
2026-08-31 18:46:12 +0530

    feat added the chatbit

--- components/TawkTo.tsx
+++ components/TawkTo.tsx
@@ -1,19 +1,25 @@
 "use client";
 
-import { useEffect } from "react";
+import Script from "next/script";
 
 export default function TawkTo() {
-  useEffect(() => {
-    var Tawk_API = (window as any).Tawk_API || {},
-      Tawk_LoadStart = new Date();
-    const s1 = document.createElement("script");
-    const s0 = document.getElementsByTagName("script")[0];
-    s1.async = true;
-    s1.src = "https://embed.tawk.to/6a954822d8a8563441ca95d7/1k1bi3i5i";
-    s1.charset = "UTF-8";
-    s1.setAttribute("crossorigin", "*");
-    s0.parentNode.insertBefore(s1, s0);
-  }, []);
-
-  return null;
+  return (
+    <Script
+      id="tawk-to"
+      strategy="lazyOnload"
+      dangerouslySetInnerHTML={{
+        __html: `
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6a954822d8a8563441ca95d7/1k1bi3i5i';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+        `,
+      }}
+    />
+  );
 }
